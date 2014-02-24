7190bb1a2036f5be0ae3b6ccc2a4e61a626fbdcf
czconnolly <c.z.connolly@dundee.ac.uk>
2014-02-24 16:45:35 +0000

    Update queries.py

--- queries.py
+++ queries.py
@@ -5,7 +5,8 @@ import cgitb
 import models
 cgitb.enable()
 
-#links the form to the models.py python script and creates a table for the queries to be submited then displayed
+#links the form to the models.py python script and creates a table for the queries to be displayed
+
 form= cgi.FieldStorage()
 print 'content-type: text/html'
 print
@@ -14,9 +15,9 @@ print '<body><h1>form values</h1>'
 form=cgi.FieldStorage()
 db=models.DBHandler()
 cursor=db.cursor()
-cursor.execute(probesql,(form[query],))
+cursor.execute(probesql,(form[query],)) #executes the models.py query using the input from the website form
 
-print form['query']
+print form['query'] #prints the result in a table
 print '<form method=POST action=models.py>'
 print '<table><tr><td>select probe names where the gene ID = </td><td><input type=text name=query /></td></tr>'
 print '</table>'
